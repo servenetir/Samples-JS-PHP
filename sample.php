6926c81d3f5c1044008b7285a00672cd6dadc27a
dijangodev <128137695+dijangodev@users.noreply.github.com>
2023-03-17 13:13:28 +0330

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,9 @@
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Helper.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Report.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Upload.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/PostType.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/MetaBox.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Settings.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/User.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Ajax.php';
